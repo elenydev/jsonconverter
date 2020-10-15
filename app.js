a126e5b945bc09951057c320b531e1d6d8978be8
Damian Czarnota <godevdamian@gmail.com>
2020-10-15 12:32:01 +0200

    refactor of file structure

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-const fs = require("fs");
-
-const csvBuildings = "BuildingsCSV.csv";
-const csvConverter = require("csvtojson");
 const createCountriesJson = require('./helpers/Countries/countriesListBuilder');
+const createBuildingsJson = require('./helpers/Buildings/buildingsListBuilder');
+const csvBuildings = "./data/Buildings/BuildingsCSV.csv";
 
-const convertToJson = async (baseFile, outputFileName) => {
-  const jsonOutput = await csvConverter()
-    .fromFile(baseFile)
-    .then((jsonObject) => {
-      const data = JSON.stringify(jsonObject, null, 2);
-
-      fs.writeFile('data/'+outputFileName, data, (err, data) => {
-        if (!err) return data;
-
-        console.log(err);
-      });
-    })
-
-    .catch((err) => {
-      console.log(err);
-    });
-};
 
 createCountriesJson()
-convertToJson(csvBuildings, "Buildings.json");
+createBuildingsJson(csvBuildings, "Buildings.json");
--- helpers/Countries/countriesListBuilder.js
+++ helpers/Countries/countriesListBuilder.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
-const countries = require('../../data/countriesListArray')
+const countries = require('../../data/Countries/countriesListArray.js')
 
 const COUNTRY_DATA_GETTERS  = require('./countriesFunctions')
 const COUNTRY_PROPERTIES = Object.keys(COUNTRY_DATA_GETTERS);
 
 const setCountryObject = (currentCountry) =>{
 
-  const country = require(`../../countriesData/${currentCountry}.json`);
+  const country = require(`../../data/Countries/countriesData/${currentCountry}.json`);
   
   const countryData = {}
 
@@ -32,7 +32,7 @@ const createCountriesJson = (countries) =>{
 
   const data = JSON.stringify(countriesData, null, 2);
 
-  fs.writeFile('data/Countries.json', data, (err) => {
+  fs.writeFile('data/Countries/Countries.json', data, (err) => {
   if (err) throw err;
   });
 
